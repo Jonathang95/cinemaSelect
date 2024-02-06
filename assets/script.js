aa98a25c9f75e72283bc99b4eb9e43337a970f56
beanbean <gracefernandez00@icloud.com>
2024-02-05 19:38:32 -0700

    1st iteration genre API

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+const url = 'https://moviesminidatabase.p.rapidapi.com/movie/byGen/%7Bgen%7D/';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8bf819c497mshd24237e2102709cp11306fjsne28ef4f0886f',
+		'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
