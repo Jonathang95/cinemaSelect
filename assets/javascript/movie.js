5f2b65ced6c59fc8580bc74f43abd529adf8ca15
beanbean <gracefernandez00@icloud.com>
2024-02-08 21:11:43 -0700

    api pulling data

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -1,20 +1,28 @@
-const url = 'https://moviesminidatabase.p.rapidapi.com/movie/byGen/%7Bgen%7D/';
+var submitButton = document.querySelector("#searchButton")
+
+async function fetchData() {
+var movienameEL = document.querySelector("#searchInput")
+var moviename = movienameEL.value
+const url = `https://imdb_api4.p.rapidapi.com/get_movies_by_name?Movie_name=${moviename}`;
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '8bf819c497mshd24237e2102709cp11306fjsne28ef4f0886f',
-		'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+		'X-RapidAPI-Host': 'imdb_api4.p.rapidapi.com'
 	}
 };
 
-async function fetchData() {
 	try {
 		const response = await fetch(url, options);
 		const result = await response.json();
+		resultsEL.innerHTML = `<h1>${result[0].title}</h1>`
 		console.log(result);
 	} catch (error) {
 		console.error(error);
 	}
 }
 
-fetchData()
+submitButton.addEventListener("click", fetchData)
+
+
+var resultsEL = document.querySelector("#TitleResults")
